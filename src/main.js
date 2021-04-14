5c94506cb3dc52bbfeb440cf77e3f80cceb2e67a
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-04-14 19:36:46 +0500

    feat: настроить часы и время, прописать фильтр

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-import './registerServiceWorker'
 import router from './router'
 import store from './store'
+// eslint-disable-next-line no-unused-vars
+import dateFilter from '@/filters/dateFilter'
+import './registerServiceWorker'
 import 'materialize-css/dist/js/materialize.min.js'
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
