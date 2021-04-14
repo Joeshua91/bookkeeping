1983c57f24219fdb0c29136485bdaf434840440c
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-04-14 14:29:53 +0500

    fix: убрать ошибку из консоли (опечатка в Sidebar), потерялся link

--- src/components/app/Sidebar.vue
+++ src/components/app/Sidebar.vue
@@ -2,7 +2,7 @@
   <ul class="sidenav app-sidenav" :class="{ open: value }">
     <router-link
       v-for="link in links"
-      :key="(link, url)"
+      :key="link.url"
       tag="li"
       active-class="active"
       :to="link.url"
