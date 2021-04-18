83776a74822a4c9dfba1abc96547ae0555b5afa9
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-04-18 13:58:03 +0500

    feat: связать новые записи с БД

--- src/views/Record.vue
+++ src/views/Record.vue
@@ -90,6 +90,7 @@
 <script>
 /* eslint-disable */
 import { required, minValue } from "vuelidate/lib/validators";
+import { mapGetters } from "vuex";
 
 export default {
   name: "record",
@@ -119,11 +120,49 @@ export default {
       window.M.updateTextFields();
     }, 0);
   },
+  computed: {
+    ...mapGetters(["info"]),
+    canCreateRecord() {
+      if (this.type === "income") {
+        return true;
+      }
+
+      return this.info.bill >= this.amount;
+    },
+  },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
+      }
+
+      if (this.canCreateRecord) {
+        try {
+          await this.$store.dispatch("createRecord", {
+            categoryId: this.category,
+            amount: this.amount,
+            description: this.description,
+            type: this.type,
+            date: new Date().toJSON(),
+          });
+          const bill =
+            this.type === "income"
+              ? this.info.bill + this.amount
+              : this.info.bill - this.amount;
+
+          await this.$store.dispatch("updateInfo", { bill });
+          this.$message("Запись успешно создана");
+          this.$v.$reset();
+          this.amount = 1;
+          this.description = "";
+        } catch (e) {}
+      } else {
+        this.$message(
+          this.$message(
+            `Недостаточно средств на счете (${this.amount - this.info.bill})`
+          )
+        );
       }
     },
   },
