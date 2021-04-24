a44768933bfb9699e57d8a789c189b1532a8f2d3
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-04-24 10:12:37 +0500

    feat: защитить роуты

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,5 +1,6 @@
 export default {
   'logout': 'Вы вышли из системы',
+  'login': 'Необходимо войти в систему',
   'auth/user-not-found': 'Пользователя с таким E-mail не существует',
   'auth/wrong-password': 'Неверный пароль',
   'auth/email-already-in-use': 'E-mail уже занят',
