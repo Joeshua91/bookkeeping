a32c0c2a85e9f0eb36e36a63cd6c88123e90f472
Mikhail Koryukov <mezhovkin@yandex.ru>
2021-04-26 12:38:23 +0500

    feat: сделать валидацию

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,11 +4,20 @@
       <h3>Профиль</h3>
     </div>
 
-    <form class="form">
+    <form class="form" @submit.prevent="submitHandler">
       <div class="input-field">
-        <input id="description" type="text" />
+        <input
+          id="description"
+          type="text"
+          v-model="name"
+          :class="{ invalid: $v.name.$dirty && !$v.name.required }"
+        />
         <label for="description">Имя</label>
-        <span class="helper-text invalid">name</span>
+        <small
+          class="helper-text invalid"
+          v-if="$v.name.$dirty && !$v.name.required"
+          >Введите имя
+        </small>
       </div>
 
       <div class="switch">
@@ -28,7 +37,42 @@
   </div>
 </template>
 
-
+<script>
+import { mapGetters, mapActions } from "vuex";
+import { required } from "vuelidate/lib/validators";
+/* eslint-disable */
+export default {
+  data: () => ({
+    name: "",
+  }),
+  validations: {
+    name: { required },
+  },
+  mounted() {
+    this.name = this.info.name;
+    setTimeout(() => {
+      window.M.updateTextFields();
+    });
+  },
+  computed: {
+    ...mapGetters(["info"]),
+  },
+  methods: {
+    ...mapActions(["updateInfo"]),
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
+      try {
+        await this.updateInfo({
+          name: this.name,
+        });
+      } catch (e) {}
+    },
+  },
+};
+</script>
 
 <style scoped>
 .switch {
